a4596ce9e3796467d3019feacbe48cb09e8174df
Deepika-S573376 <divyagnanadeepika.m24@gmail.com>
2024-10-31 16:21:35 -0500

    added colours to text

--- app/src/main/java/com/example/taskmanager/edit_Task_Activity.java
+++ app/src/main/java/com/example/taskmanager/edit_Task_Activity.java
@@ -1,24 +1,45 @@
 package com.example.taskmanager;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class edit_Task_Activity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editET, editET2, editET3, editET4;
+    private Button buttonBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_edit_task);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main);
+
+        // Initializing EditText and Button
+        editET = findViewById(R.id.editET);
+        editET2 = findViewById(R.id.editET2);
+        editET3 = findViewById(R.id.editET3);
+        editET4 = findViewById(R.id.editET4);
+        buttonBTN = findViewById(R.id.buttonBTN);
+
+        // Setting up button click listener
+        buttonBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Getting the input text from the EditTexts
+                String label = editET.getText().toString();
+                String date = editET2.getText().toString();
+                String time = editET3.getText().toString();
+                String description = editET4.getText().toString();
+
+                // Displaying the input data as a Toast message
+                String message = "Label: " + label + "\nDate: " + date + "\nTime: " + time + "\nDescription: " + description;
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
