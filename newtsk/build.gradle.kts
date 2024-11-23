94009aacd500769ba66d44f0227d3eaec02eae00
Kalyan Chakravarthy Jogula <jogulakalyan8@gmail.com>
2024-11-22 23:37:05 -0600

    Add files via upload

--- newtsk/build.gradle.kts
+++ newtsk/build.gradle.kts
@@ -1,46 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application)
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.example.newtsk"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.newtsk"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
-
-dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    implementation(libs.firebase.auth)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    alias(libs.plugins.android.application) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
