c3d1b7fb8c6ac510022609a10da30581f8ed14db
Kalyan Chakravarthy Jogula <jogulakalyan8@gmail.com>
2024-11-22 23:04:02 -0600

    Kalyan Chakravarthy Jogula,Added all the final layouts and java files without any app crashes and with updated logo

---
+++ app/src/main/java/java/com/example/activity_login.java
@@ -0,0 +1,68 @@
+package com.example.newtsk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class activity_login extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText userNameET, passwordET;
+    Button loginBTN, registerBTN;           
+    String username, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        userNameET = findViewById(R.id.editTextUsername);
+        passwordET = findViewById(R.id.editTextPassword);
+        loginBTN = findViewById(R.id.buttonLogin);
+        registerBTN = findViewById(R.id.buttonRegister);
+
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Here, you should implement actual login logic
+                username = userNameET.getText().toString().trim();
+                password = passwordET.getText().toString().trim();
+                mAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d("Login","Login is Success");
+                            Intent intent = new Intent(activity_login.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(activity_login.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+        registerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_login.this, activity_registration.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
